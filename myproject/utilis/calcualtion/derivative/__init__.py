75d74bbe8f94e081fd1ed046edef6f77fe5bdacf
Klaudiusz321 <claudiuswebdesign@gmail.com>
2025-02-26 17:59:05 +0100

    Update Procfile configura

--- myproject/utilis/calcualtion/derivative/__init__.py
+++
@@ -1,5 +0,0 @@
-from .numeric_derivate import numeric_derivative
-from .partial_derivate import partial_derivative
-from .total_derivative import total_derivative
-
-__all__ = ['numeric_derivative', 'partial_derivative', 'total_derivative'] 
