70eac4c7889875ea04398f75f757974e15569d41
Klaudiusz321 <claudiuswebdesign@gmail.com>
2025-05-02 21:59:26 +0200

    admin django super user added

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tensor
 
 # Register your models here.
+@admin.register(Tensor)
+class TensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tensor(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    components = models.JSONField(default=dict)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
